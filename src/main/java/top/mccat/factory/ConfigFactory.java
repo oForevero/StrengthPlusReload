f4c7348c44bab529b546e6e9cb0a7168b6450225
raven <740585947@qq.com>
2022-09-08 15:01:25 +0800

    init project

--- src/main/java/top/mccat/factory/ConfigFactory.java
+++ src/main/java/top/mccat/factory/ConfigFactory.java
@@ -1,13 +1,25 @@
 package top.mccat.factory;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * @author Raven
  * @date 2022/09/05 15:44
  */
 public class ConfigFactory {
+    private final FileConfiguration yamlConfiguration = new YamlConfiguration();
     private ConfigFactory(){
 
     }
 
+    public static ConfigFactory newInstance(JavaPlugin javaPlugin) {
+        return new ConfigFactory();
+    }
 
+    public boolean configFileIsExist(){
+
+    }
 }
