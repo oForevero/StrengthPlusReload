dc1ca6d22c982fdb321d2460e6c6081dba4a7f13
Distance <740585947@qq.com>
2022-09-22 20:27:57 +0800

    实现lore基础工具

--- src/main/java/top/mccat/utils/LoreGenerateUtils.java
+++ src/main/java/top/mccat/utils/LoreGenerateUtils.java
@@ -36,40 +36,50 @@ public class LoreGenerateUtils {
     /**
      * 生成lore属性方法
      * @param level 等级
-     * @param especialAttributes 特殊LoreList
-     * @param baseAttribute 基础LoreList
+     * @param itemAttributeLore 特殊LoreList
      * @param strengthType 强化类型
      * @return loreList
      */
-    private List<String> generateAttributesLore(int level, List<Attribute> especialAttributes, String baseAttribute, StrengthType strengthType){
-        if(level == 0 && especialAttributes == null){
+    private List<String> generateAttributesLore(int level, List<String> itemAttributeLore, String setAttribute, StrengthType strengthType){
+        if(level == 0 && itemAttributeLore == null){
             List<String> dataList = new ArrayList<String>();
             switch (strengthType.getType()){
                 case 0:
-                    dataList.add(strengthAttribute.getArmorDefence()+" &3"+RomaMathGenerateUtil.intToRomanString(level));
+                    dataList.add(strengthAttribute.getArmorDefence()+" &c[&b"+RomaMathGenerateUtil.intToRomanString(level)+"&c]");
                     break;
                 case 1:
-                    dataList.add(strengthAttribute.getMeleeDamage()+" &3"+RomaMathGenerateUtil.intToRomanString(level));
+                    dataList.add(strengthAttribute.getMeleeDamage()+" &c[&b"+RomaMathGenerateUtil.intToRomanString(level)+"&c]");
                     break;
                 case 2:
-                    dataList.add(strengthAttribute.getRemotelyDamage()+" &3"+RomaMathGenerateUtil.intToRomanString(level));
+                    dataList.add(strengthAttribute.getRemotelyDamage()+" &c[&b"+RomaMathGenerateUtil.intToRomanString(level)+"&c]");
                     break;
                 default:
                     break;
             }
             return generateStrengthLore(dataList);
         }
-        switch(strengthType.getType()){
-            case 0:
-
-                break;
-            default:
-                break;
+        int indexOfAttribute = getIndexOfAttribute(itemAttributeLore , strengthType, level);
+        if(indexOfAttribute == -1){
+            return itemAttributeLore;
         }
-        return null;
+        itemAttributeLore.set(indexOfAttribute, setAttribute+"&c[&b" + level + "&c]");
+        return itemAttributeLore;
     }
 
-    public void reloadBaseConfig() {
+    private int getIndexOfAttribute(List<String> strengthAttributes, StrengthType strengthType, int level) {
+        switch (strengthType.getType()) {
+            case 0:
+                return strengthAttributes.indexOf(strengthAttribute.getDefenceAttribute() + "&c[&b" + level + "&c]");
+            case 1:
+                return strengthAttributes.indexOf(strengthAttribute.getMeleeDamage() + "&c[&b" + level + "&c]");
+            case 2:
+                return strengthAttributes.indexOf(strengthAttribute.getRemotelyDamage() + "&c[&b" + level + "&c]");
+            default:
+                return -1;
+        }
+    }
+
+    public void reloadBaseConfig () {
         strengthAttribute = StrengthAttribute.newInstance();
     }
 }
