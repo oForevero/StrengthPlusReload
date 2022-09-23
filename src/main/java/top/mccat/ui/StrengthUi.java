0137b38329157c235ec3898919fddc746e7b11aa
raven <740585947@qq.com>
2022-09-23 17:31:40 +0800

    repair YamlLoadUtils.loadConfigObject error

--- src/main/java/top/mccat/ui/StrengthUi.java
+++ src/main/java/top/mccat/ui/StrengthUi.java
@@ -1,8 +1,78 @@
 package top.mccat.ui;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import top.mccat.pojo.bean.StrengthStone;
+import top.mccat.pojo.config.StrengthMenu;
+import top.mccat.utils.ColorParseUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @author Distance
  * @date 2022/9/8
  */
 public class StrengthUi {
+    private StrengthMenu strengthMenu;
+    private final ItemStack displayBar = new ItemStack(Material.PAINTING);
+    private final ItemStack air = new ItemStack(Material.AIR);
+    private final ItemStack progressBar = new ItemStack(Material.WHITE_STAINED_GLASS_PANE);
+    private final ItemStack runningBar = new ItemStack(Material.LIME_STAINED_GLASS_PANE);
+    private final ItemStack successProgressBar = new ItemStack(Material.YELLOW_STAINED_GLASS_PANE);
+    private final ItemStack failProgressBar = new ItemStack(Material.PINK_STAINED_GLASS_PANE);
+    private final ItemStack strengthDividerGlass = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
+    private final ItemStack enchantingTable = new ItemStack(Material.ENCHANTING_TABLE);
+    private final ItemStack ironBars = new ItemStack(Material.IRON_BARS);
+    private final ItemStack fire = new ItemStack(Material.SOUL_CAMPFIRE);
+    private final ItemStack startButton = new ItemStack(Material.END_CRYSTAL);
+    private final ItemStack extraTable = new ItemStack(Material.END_PORTAL_FRAME);
+    private final ItemStack closeMenu = new ItemStack(Material.BARRIER);
+    private Thread uiSonThread;
+    private List<StrengthStone> stoneList;
+    private final int inventorySize = 54;
+    Map<Player,Boolean> playerInMenuMap = new HashMap(100);
+
+    /**
+     * 整体ui数组，特殊按钮等用air itemstack填充，强化物品放置栏用
+     */
+    private final ItemStack[] STRENGTH_UI = new ItemStack[]
+            {strengthDividerGlass,strengthDividerGlass,strengthDividerGlass,ironBars,ironBars,ironBars,ironBars,strengthDividerGlass,displayBar,
+                    strengthDividerGlass,strengthDividerGlass,strengthDividerGlass,ironBars,air,air,ironBars,strengthDividerGlass,strengthDividerGlass,
+                    strengthDividerGlass,air,strengthDividerGlass,ironBars,ironBars,ironBars,ironBars,strengthDividerGlass,air,
+                    strengthDividerGlass,enchantingTable,strengthDividerGlass,ironBars,fire,fire,ironBars,strengthDividerGlass,extraTable,
+                    strengthDividerGlass,strengthDividerGlass,strengthDividerGlass,strengthDividerGlass,strengthDividerGlass,strengthDividerGlass,strengthDividerGlass,startButton,strengthDividerGlass,
+                    progressBar,progressBar,progressBar,progressBar,progressBar,progressBar,progressBar,strengthDividerGlass,closeMenu};
+
+    public StrengthUi() {
+        setItemName(enchantingTable,"&b强化物品台");
+        setItemName(fire,"&5灵火锻造");
+        setItemName(progressBar,"&a进度条");
+        setItemName(ironBars,"&8分割线");
+        setItemName(strengthDividerGlass, "&8分隔板");
+        setItemName(startButton,"&c开始强化");
+        setItemName(extraTable,"&b附加物品台");
+        setItemName(displayBar, "&a强化信息");
+        setItemName(closeMenu, "&c关闭菜单");
+    }
+
+    public static StrengthUi newInstance() {
+        return new StrengthUi();
+    }
+
+    /**
+     * 设置物品名
+     * @param stack itemStack对象
+     * @param name 物品名
+     */
+    private void setItemName(ItemStack stack, String name){
+        ItemMeta itemMeta = stack.getItemMeta();
+        assert itemMeta != null;
+        itemMeta.setDisplayName(ColorParseUtils.parseColorStr(name));
+        stack.setItemMeta(itemMeta);
+    }
 }
